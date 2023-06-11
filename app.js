2a6f0c51614ce7917af04b0bfc054a4e2abf2880
afsalafi <afsalpt956786@gmail.com>
2023-06-11 13:48:50 +0530

    changes

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ import dotenv  from 'dotenv';
 import connectDb from './connection/dbConnection.js';
 import userRouter from './routes/user.js'
 import cors from 'cors'
-// import { fileURLToPath } from 'url';
-// import { dirname, join } from 'path';
 import path from 'path';
 
 const __dirname = path.resolve();
@@ -16,14 +14,12 @@ let data_base=process.env.DATABASE_CONNECTION;
 connectDb(data_base);
 
 app.use('/uploads', express.static(path.join(__dirname ,'/uploads')))
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-// app.use('/uploads', express.static(join(dirname(__dirname), '/uploads')));
+
 
 app.use(express.json());
 app.use(cors({
     credentials:true,
-    origin:'http://localhost:5173'
+    origin:'https://react-usermanagement.onrender.com'
 }))
 
 
