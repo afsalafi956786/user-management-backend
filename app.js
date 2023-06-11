9f0e4663b898af12dbad58b5affb454d04355f38
afsalafi <afsalpt956786@gmail.com>
2023-06-11 14:24:04 +0530

    cors fix

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use('/uploads', express.static(path.join(__dirname ,'/uploads')))
 app.use(express.json());
 app.use(cors({
     credentials:true,
-    origin:'http://localhost:5173'
+    origin:'https://react-usermanagement.onrender.com'
     
    
 }))
